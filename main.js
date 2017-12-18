52e3c9a6aab8c3fcff7e13623b46bb50add3ea7e
JKarkosza <jakubkarkosza@gmail.com>
2017-12-18 10:54:19 +0100

    html & css

---
+++ main.js
@@ -0,0 +1,9 @@
+alert('Welcome to Quiz Ninja');
+
+const question = "What is Superman's real name?";
+const answer = prompt(question);
+alert(`You answered ${answer}`);
+
+const heroes = ['bat', 'mat', 'spidi'];
+const avengers = ['Capitan Ameryka', 'Iron Man', 'Thor', 'Hulk', 'Hawkeye', 'Black Widow'];
+console.log(avengers.length);
