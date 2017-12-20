f3662e02bea4d4984250adc203707833ad513211
JKarkosza <jakubkarkosza@gmail.com>
2017-12-20 15:46:25 +0100

    JS questions & score

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-alert('Welcome to Quiz Ninja');
+document.addEventListener('DOMContentLoaded', function(){
 
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+  alert('Welcome to Quiz Ninja');
 
-const heroes = ['bat', 'mat', 'spidi'];
-const avengers = ['Capitan Ameryka', 'Iron Man', 'Thor', 'Hulk', 'Hawkeye', 'Black Widow'];
-console.log(avengers.length);
+  const quiz = [
+    ["What is Superman's real name?", "Clark Kent"],
+    ["What is Wonder Woman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"]
+  ];
+
+  let score = 0;
+
+  for( const [question, answer] of quiz){
+    const response = prompt( question );
+    if( response === answer) {
+      alert('Correct');
+      score++;
+    } else {
+      alert(`Wrong! The correct answer was ${answer}`);
+    }
+  };
+
+  alert(`Game Over, you scored ${score} point${score !==1 ? 's' : ''}`);
+
+
+const tablica = [
+  [1,2],
+  [3,4],
+  [5,6]
+];
+
+for(const [val1, val2] of tablica) {
+  console.log(val1 + val2);
+}
+
+
+
+
+
+
+});
